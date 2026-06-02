7cb8ad4807dc843938b72b927996a518baa19638
serr <sc7227484@gmail.com>
2026-06-02 21:32:02 +0300

    refactor: simplify emado-mode and remove compilation-mode dependencies

--- emado.el
+++ emado.el
@@ -21,6 +21,60 @@
                  (directory :tag "Project root"))
   :group 'emado)
 
+(defun emado-next-line ()
+  "Move to next logical line."
+  (interactive)
+  (forward-line 1))
+
+(defun emado-previous-line ()
+  "Move to previous logical line."
+  (interactive)
+  (forward-line -1))
+
+(defun emado-quit ()
+  "Close emado window and bury buffer."
+  (interactive)
+  (quit-window))
+
+(defun emado-open-file-at-point ()
+  "Open file at current line."
+  (interactive)
+  (save-excursion
+    (beginning-of-line)
+    (when (looking-at "^\\(.*\\):\\([0-9]+\\):\\([0-9]+\\):")
+      (let ((file (match-string 1))
+            (line (string-to-number (match-string 2)))
+            (col (string-to-number (match-string 3))))
+        (find-file-other-window file)
+        (goto-char (point-min))
+        (forward-line (1- line))
+        (forward-char (1- col))))))
+
+(defvar emado-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "n") 'emado-next-line)
+    (define-key map (kbd "p") 'emado-previous-line)
+    (define-key map (kbd "q") 'emado-quit)
+    (define-key map (kbd "RET") 'emado-open-file-at-point)
+    map)
+  "Keymap for emado buffer.")
+
+(define-derived-mode emado-mode fundamental-mode "Emado"
+  "Major mode for viewing mado output."
+  (read-only-mode 1)
+  (use-local-map emado-mode-map))
+
+(defun emado--display (output)
+  "Display OUTPUT."
+  (let ((buf (get-buffer-create "*emado*")))
+    (with-current-buffer buf
+      (let ((inhibit-read-only t))
+        (erase-buffer)
+        (insert output)
+        (goto-char (point-min))
+        (emado-mode)))
+    (pop-to-buffer buf)))
+
 ;;;###autoload
 (defun emado-run (args &optional directory)
   "Run mado with ARGS (list of strings) in DIRECTORY."
@@ -31,34 +85,25 @@
       (with-current-buffer standard-output
         (apply #'call-process emado-executable nil t nil args)))))
 
-(defun emado--display (output)
-  "Display OUTPUT in compilation buffer."
-  (with-current-buffer (get-buffer-create "*emado*")
-    (let ((inhibit-read-only t))
-      (erase-buffer)
-      (insert output)
-      (compilation-mode)
-      (goto-char (point-min)))
-    (pop-to-buffer (current-buffer))))
-
 ;;;###autoload
 (defun emado-print (query)
-  "Print entries matching QUERY in a compilation buffer."
+  "Print entries matching QUERY."
   (interactive "sQuery: ")
-  (emado--display (emado-run (list "-p" query))))
+  (let ((output (emado-run (list "-p" query))))
+    (emado--display output)))
 
 ;;;###autoload
 (defun emado-remove (query)
-  "Remove entries matching QUERY in a compilation buffer."
+  "Remove entries matching QUERY."
   (interactive "sQuery: ")
-  (emado--display (emado-run (list "-r" query))))
+  (let ((output (emado-run (list "-r" query))))
+    (emado--display output)))
 
 ;; Transient menu
 
 (transient-define-prefix emado-menu ()
   "Mado entry manager."
   [["Query"
-    ("a" "all entries" (lambda () (interactive) (emado-print "all")))
     ("p" "print by query" (lambda () (interactive)
                             (emado-print (read-string "Query (print): "))))
     ("r" "remove by query" (lambda () (interactive)
